1d1f92f02a9a28b91326c1fc052d0231cc01deed
Stephen Lewis <stephen@experiencehq.net>
2015-01-21 00:08:19 +0000

    Permit key with the same name as a protected property.

--- src/SelvinOrtiz/Zit/Zit.php
+++ src/SelvinOrtiz/Zit/Zit.php
@@ -1,6 +1,9 @@
 <?php
 namespace SelvinOrtiz\Zit;
 
+use ReflectionClass;
+use ReflectionProperty;
+
 /**
  * @=SelvinOrtiz\Zit
  *
@@ -20,7 +23,7 @@ class Zit implements IZit
 	protected $services		= array();
 	protected $callables	= array();
 
-	protected function __construct() 	{}
+	protected function __construct()	{}
 	protected function __clone()		{}
 
 	/**
@@ -108,14 +111,16 @@ class Zit implements IZit
 
 	public function __get($id)
 	{
-		if (property_exists($this, $id))
-		{
-			return $this->{$id};
+		$reflector = new ReflectionClass($this);
+		$properties = $reflector->getProperties(ReflectionProperty::IS_PUBLIC);
+
+		foreach ($properties as $property) {
+			if ($property->getName() === $id) {
+				return $this->{$id};
+			}
 		}
-		else
-		{
-			return $this->pop($id);
-		}
+
+		return $this->pop($id);
 	}
 
 	public function __call($id, $args=array())
